c0cf6b2f43bfed3f2861ece9920e9fa8f1b9e39f
Adrodoc <adrodoc55@googlemail.com>
2019-11-08 13:37:12 +0100

    Verbesserung von worksheet02

--- worksheet02/main.cpp
+++ worksheet02/main.cpp
@@ -5,24 +5,33 @@ double** matrix_new(int rows, int cols);
 void matrix_delete(double** matrix);
 
 int main() {
-	double** my_matrix = matrix_new(10, 120);
-	my_matrix[4][7] = 5.0;
-	printf("%f", my_matrix[4][7]);
+	int rows = 5;
+	int cols = 8;
+	double** my_matrix = matrix_new(rows, cols);
+	for (int r = 0; r < rows; r++) {
+		for (int c = 0; c < cols; c++) {
+			my_matrix[r][c] = r * cols + c;
+		}
+	}
+	for (int r = 0; r < rows; r++) {
+		for (int c = 0; c < cols; c++) {
+			printf("%f\t", my_matrix[r][c]);
+		}
+		printf("\n");
+	}
 	matrix_delete(my_matrix);
 }
 
 double** matrix_new(int rows, int cols) {
 	double** matrix = (double**)malloc(rows * sizeof(double*));
+	double* rowPointer = (double*)malloc(rows * cols * sizeof(double));
 	for (int i = 0; i < rows; i++) {
-		matrix[i] = (double*)malloc(cols * sizeof(double));
+		matrix[i] = &rowPointer[i * cols];
 	}
 	return matrix;
 }
 
 void matrix_delete(double** matrix) {
-	int rows = 10;
-	for (int i = 0; i < rows; i++) {
-		free(matrix[i]);
-	}
+	free(*matrix);
 	free(matrix);
 }
